d46e54f5b7e5fbf6a835a04faf9134865653cab5
Bruno Dias <brunodg.dev@gmail.com>
2026-05-07 15:00:53 -0300

    [update] Added new enterprised I've worked before to the platform.

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -1,17 +1,21 @@
-import MedgrupoLogo from '@/assets/images/clients/Medgrupo_Logo.png'
+import MedgrupoLogo from "@/assets/images/clients/Medgrupo_Logo.png";
 
 export interface Client {
-    name: string;
-    logo: string;
+  name: string;
+  logo: string;
 }
 
 export const clients: Client[] = [
-    {
-        name: 'FS Studio',
-        logo: 'https://fsstudio.com/wp-content/themes/fs-studio/assets/img/svg/logo.svg'
-    },
-    {
-        name: 'MedGrupo',
-        logo: MedgrupoLogo
-    }
-]; 
+  {
+    name: "FS Studio",
+    logo: "https://fsstudio.com/wp-content/uploads/2025/11/Logo-fs.webp",
+  },
+  {
+    name: "MedGrupo",
+    logo: MedgrupoLogo,
+  },
+  {
+    name: "Instituto Tecgraf / PUC-Rio",
+    logo: "https://www.tecgraf.puc-rio.br/images/svg/logo.svg",
+  },
+];
